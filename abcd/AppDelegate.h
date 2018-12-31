b18daea15a9fe779d07901b4a442b340721eaee8
Shiv <shivshankar938@gmail.com>
2018-12-31 09:14:36 +0530

    Initial Commit

---
+++ abcd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  abcd
+//
+//  Created by Little on 31/12/18.
+//  Copyright © 2018 Nitish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ abcd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  abcd
+//
+//  Created by Little on 31/12/18.
+//  Copyright © 2018 Nitish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
